91b4fabd7bd3ff93e5967717f745efb1a7318a6c
UPJM <44162070+UPJM@users.noreply.github.com>
2019-04-03 17:27:19 +0100

    added JSON dummy data and working functionality to import to add calendar event
    created JSON file that stores values locally and maintains the data even when the session has ended or server has terminated. This will be incorporated into adding events

--- server.js
+++ server.js
@@ -2,15 +2,54 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
+let data = fs.readFileSync('words.json');
+let words = JSON.parse(data);
+
+console.log(words);
 
 app.use(express.static('public'));
 
 app.get('/',function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
 app.get('/css/style.css',function(req, res){
   res.sendFile(__dirname + '/css/style.css');
 });
+
+
+app.get('/add/:word/:score?', addWord);
+function addWord(request,response){
+ let data = request.params;
+ let word = data.word;
+ let score = Number(data.score);
+ if (!score){
+   var reply = {
+     msg: "score required"
+   }
+   response.send(reply);
+ } else {
+   words[word] = score;
+   let data = JSON.stringify(words, null, 2);
+   fs.writeFile('words.json', data, finished);
+   function finished(err){
+     console.log('all set...');
+     reply = {
+       word: word,
+       score: score,
+      status: "success"
+     }
+     response.send(reply);
+   }
+
+
+}
+}
+
+
+app.get('/all', sendAll);
+function sendAll(request,response){
+  response.send(words);
+}
 
 app.listen(8080);
