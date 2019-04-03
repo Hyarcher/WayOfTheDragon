5a7a5a2475ebc41c79354d118179d09f804d9b4f
Hyarcher <HyarcherHyarcher@gmail.com>
2019-04-03 18:25:02 +0100

    Changed the style of the pages in CSS
    Formatted the Calendar.js and index.css

--- public/Calendar.js
+++ public/Calendar.js
@@ -9,25 +9,25 @@ const months = ["January", "Febuary", "March", "April", "May", "June", "July", "
 let monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
-function next() {
+function next(){
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
     showCalendar(currentMonth, currentYear);
 }
 
-function previous() {
+function previous(){
     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
     showCalendar(currentMonth, currentYear);
 }
 
-function jump() {
+function jump(){
     currentYear = parseInt(selectYear.value);
     currentMonth = parseInt(selectMonth.value);
     showCalendar(currentMonth, currentYear);
 }
 
-function showCalendar(month, year) {
+function showCalendar(month, year){
 
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
@@ -41,25 +41,25 @@ function showCalendar(month, year) {
     selectMonth.value = month;
 
     let date = 1;
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++){
 
         let row = document.createElement("tr");
 
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
+        for (let j = 0; j < 7; j++){
+            if (i === 0 && j < firstDay){
                 let cell = document.createElement("td");
                 let cellText = document.createTextNode("");
                 cell.appendChild(cellText);
                 row.appendChild(cell);
             }
-            else if (date > daysInMonth) {
+            else if (date > daysInMonth){
                 break;
             }
 
             else {
                 let cell = document.createElement("td");
                 let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()){
                     cell.classList.add("bg-info");
                 }
                 cell.appendChild(cellText);
