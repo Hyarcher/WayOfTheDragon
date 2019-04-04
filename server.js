93046075a767b67e580d388834c0e3766fe689d1
UPJM <44162070+UPJM@users.noreply.github.com>
2019-04-04 19:27:08 +0100

    updates so database works as much as possible using routes

--- server.js
+++ server.js
@@ -40,34 +40,34 @@ function addMovie(request,response){
   });
 }
 
-//
-// app.get('/add/:word/:score?', addWord);
-// function addWord(request,response){
-//  let data = request.params;
-//  let word = data.word;
-//  let score = Number(data.score);
-//  if (!score){
-//    var reply = {
-//      msg: "number required"
-//    }
-//    response.send(reply);
-//  } else {
-//    words[word] = score;
-//    let data = JSON.stringify(words, null, 2);
-//    fs.writeFile('words.json', data, finished);
-//    function finished(err){
-//      console.log('all set...');
-//      reply = {
-//        word: word,
-//        score: score,
-//       status: "success"
-//      }
-//      response.send(reply);
-//    }
-//
-//
-// }
-// }
+
+ app.get('/add/:word/:score?', addWord);
+ function addWord(request,response){
+  let data = request.params;
+  let word = data.word;
+  let score = Number(data.score);
+  if (!score){
+    var reply = {
+      msg: "number required"
+    }
+   response.send(reply);
+  } else {
+    words[word] = score;
+    let data = JSON.stringify(words, null, 2);
+  fs.writeFile('words.json', data, finished);
+function finished(err){
+      console.log('all set...');
+      reply = {
+        word: word,
+        score: score,
+       status: "success"
+      }
+      response.send(reply);
+    }
+
+
+ }
+ }
 
 
 app.get('/all', sendAll);
