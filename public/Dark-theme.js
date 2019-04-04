9782977f4fd9fa07c235eccb5fb10a5ad3bcb83a
gwhizzle <georgeavery28@gmail.com>
2019-04-04 21:10:34 +0100

    Updating Application Settings
    Added 'Save and Exit' button

--- public/Dark-theme.js
+++ public/Dark-theme.js
@@ -1,3 +1,23 @@
+function save() {
+	var checkbox = document.getElementById("checkbox1");
+    localStorage.setItem("checkbox1", checkbox.checked);
+  var checkbox = document.getElementById("checkbox2");
+    localStorage.setItem("checkbox2", checkbox.checked);
+}
+
+var checked = JSON.parse(localStorage.getItem("checkbox1"));
+    document.getElementById("checkbox1").checked = checked;
+
+var checked = JSON.parse(localStorage.getItem("checkbox2"));
+    document.getElementById("checkbox2").checked = checked;
+
+if (document.getElementById('checkbox1').checked){
+    changeCSS('darkstyle.css', 0);
+}else if (document.getElementById('checkbox2').checked) {
+    changeCSS('style.css', 0);
+}
+
+
 function changeCSS(cssFile, cssLinkIndex) {
 
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
