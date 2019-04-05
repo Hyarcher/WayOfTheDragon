bd02d9d81890e40d07e9870659ebcc73b056b7ba
gwhizzle <georgeavery28@gmail.com>
2019-04-05 12:35:41 +0100

    Added Comments to Dark-theme.js

--- public/scripts/Dark-theme.js
+++ public/scripts/Dark-theme.js
@@ -1,3 +1,5 @@
+
+//Saves which radio button is checked to the localStorage
 function save() {
 	var checkbox = document.getElementById("checkbox1");
     localStorage.setItem("checkbox1", checkbox.checked);
@@ -5,19 +7,20 @@ function save() {
     localStorage.setItem("checkbox2", checkbox.checked);
 }
 
+//Reterives the localStorage and keeps the radio button checked
 var checked = JSON.parse(localStorage.getItem("checkbox1"));
     document.getElementById("checkbox1").checked = checked;
-
 var checked = JSON.parse(localStorage.getItem("checkbox2"));
     document.getElementById("checkbox2").checked = checked;
 
+//Uses the radio button inputs to call the changeCSS(); function and select the css file
 if (document.getElementById('checkbox1').checked){
     changeCSS('darkstyle.css', 0);
 }else if (document.getElementById('checkbox2').checked) {
     changeCSS('style.css', 0);
 }
 
-
+//Changes the css that is referneced
 function changeCSS(cssFile, cssLinkIndex) {
 
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
