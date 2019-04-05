d56b6d2f3cf6bb58037d1e985560acd320c1c6d1
UPJM <44162070+UPJM@users.noreply.github.com>
2019-04-05 12:22:56 +0100

    added comments to code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Creating Neccessary Variables for server
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -7,30 +8,36 @@ let data = fs.readFileSync('words.json');
 let words = JSON.parse(data);
 let bodyParser = require('body-parser');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+
+ // for parsing application/json
+ // for parsing application
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//testing purposes, logging data to terminal
 console.log(words);
 
+//HTTP method that the middleware function handles
 app.use(express.static('public'));
 
+//GET method for sending the index file on startup Localhost
 app.get('/',function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//GET method for CSS design
 app.get('/css/style.css',function(req, res){
   res.sendFile(__dirname + '/css/style.css');
 });
 
+//POST method for adding the data to JSON, unfotunately implementation failed as data wasnt storing persistantly and was overwriting
 app.post('/movies', addMovie);
-
 function addMovie(request,response){
   console.log("Adding movie",request.body);
-
-  //let data = JSON.stringify(movie, null, 2);
   fs.writeFile('words.json', JSON.stringify(request.body),  (err) => {
     if (err) {
-      console.log('Save fsiledf!');
+      console.log('Save failed!');
       response.json(false);
       throw err;
     } else {
@@ -40,7 +47,8 @@ function addMovie(request,response){
   });
 }
 
-
+//GET method that allows a user to pass in variables that are added to JSON, slightly abstract but fully functional.
+//Cathes the error if user does not add a time variable as require and responds by Prompting the user to add time
  app.get('/add/:word/:score?', addWord);
  function addWord(request,response){
   let data = request.params;
@@ -69,7 +77,7 @@ function finished(err){
  }
  }
 
-
+//GET method that returns all data inside the words parameter (JSON data)
 app.get('/all', sendAll);
 function sendAll(request,response){
   response.send(words);
